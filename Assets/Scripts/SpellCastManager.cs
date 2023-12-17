a51d792fd3ddd55f14f89f21a09195a9246c6d71
umitceeelik <umitclk2001@gmail.com>
2023-12-17 03:53:06 +0300

    Voice Recognition Added

--- Assets/Scripts/SpellCastManager.cs
+++ Assets/Scripts/SpellCastManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Oculus.Voice;
+using Meta.WitAi.Json;
 
 public class SpellCastManager : MonoBehaviour
 {
     public InputActionProperty spellInput;
     public List<SpellData> spells;
     public MovementRecognizer movementRecognizer;
+    public AppVoiceExperience voiceRecognizer;
+
+    private string saidSpell;
 
     [System.Serializable]
     public class SpellData
@@ -15,13 +20,21 @@ public class SpellCastManager : MonoBehaviour
         public BasicSpell spell;
         public bool useMovement;
         public string movementName;
+        public bool useVoice;
+        public string voiceName;
     }
 
     public BasicSpell defaultSpell;
     public Transform wandTip;
     void Start()
     {
-        
+        voiceRecognizer.VoiceEvents.OnPartialResponse.AddListener(SetSaidSpeell);
+    }
+
+    private void SetSaidSpeell(WitResponseNode response)
+    {
+        string intentName = response["intents"][0]["name"].Value.ToString();
+        saidSpell = intentName;
     }
 
     // Update is called once per frame
@@ -33,7 +46,7 @@ public class SpellCastManager : MonoBehaviour
         }
         else if(spellInput.action.WasReleasedThisFrame())
         {
-            StopCasting();
+            StartCoroutine(StopCasting());
         }
 
         
@@ -42,15 +55,22 @@ public class SpellCastManager : MonoBehaviour
     public void StartCasting()
     {
         movementRecognizer.StartMovement();
+        voiceRecognizer.Activate();
+        saidSpell = "";
     }
 
-    public void StopCasting()
+    public IEnumerator StopCasting()
     {
         string movementName = movementRecognizer.EndMovement();
+        voiceRecognizer.Deactivate();
+
+        yield return new WaitForSeconds(0.35f);
+
         SpellData spellToCast;
 
         if (movementName != null)
         {
+            Debug.Log(movementName);
             int spellToCastIndex = spells.FindIndex(x => x.movementName == movementName && x.useMovement);
 
             if(spellToCastIndex >= 0)
@@ -61,6 +81,20 @@ public class SpellCastManager : MonoBehaviour
             {
                 spellToCast = spells[0];
             }
+        }
+        else if(saidSpell != null && saidSpell != "")
+        {
+            Debug.Log(saidSpell);
+            int spellToCastIndex = spells.FindIndex(x => x.voiceName == saidSpell && x.useVoice);
+
+            if (spellToCastIndex >= 0)
+            {
+                spellToCast = spells[spellToCastIndex];
+            }
+            else
+            {
+                spellToCast = spells[0];
+            } 
         }
         else
         {
